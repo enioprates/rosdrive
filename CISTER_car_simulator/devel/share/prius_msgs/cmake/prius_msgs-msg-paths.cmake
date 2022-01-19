d0d246245692b97e5e4ef3db23a5ddd1af888b21
enioprates <enioprates@gmail.com>
2022-01-19 11:21:15 +0000

    Delete CISTER_car_simulator/devel directory

--- CISTER_car_simulator/devel/share/prius_msgs/cmake/prius_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(prius_msgs_MSG_INCLUDE_DIRS "/home/enio/OneDrive/Cister/ROS/CarSim/CISTER_car_simulator/src/prius_msgs/msg")
-set(prius_msgs_MSG_DEPENDENCIES std_msgs)
